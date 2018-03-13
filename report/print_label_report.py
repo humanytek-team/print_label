f7243eb96c136463f7f7669b556f5613618bac62
Ruben Dario Bravo <rbravo@hoatzin.org>
2018-03-13 07:21:04 -0400

    cambios a consultas sql

--- report/print_label_report.py
+++ report/print_label_report.py
@@ -72,13 +72,92 @@ class PrintLabelProd2Mrp(models.AbstractModel):
         return int(num)
 
     def get_date(self, mrp_id):
-        StockMove = self.env['stock.move']
-        moves = StockMove.search([('production_id.id', '=', mrp_id)])
-        if moves:
-            if moves[0].move_dest_id:
-                if moves[0].move_dest_id.picking_id:
-                    return moves[0].move_dest_id.picking_id.min_date
+        #StockMove = self.env['stock.move']
+        #moves = StockMove.search([('production_id.id', '=', mrp_id)])
+        #if moves:
+            #if moves[0].move_dest_id:
+                #if moves[0].move_dest_id.picking_id:
+                    #return moves[0].move_dest_id.picking_id.min_date
+        self.env.cr.execute("""SELECT sp.min_date
+                  FROM stock_move sm
+                  JOIN stock_move smd ON smd.id = sm.move_dest_id
+                  JOIN stock_picking sp ON sp.id = smd.picking_id
+                  WHERE sm.production_id = %s;
+            """, (mrp_id,))
+        dates = self.env.cr.fetchone()
+        if dates:
+            return dates[0]
         return ''
+
+    def get_data(self, mrp_id):
+        self.env.cr.execute("""SELECT sp.min_date, sot.name, rp.name,
+                            pp.default_code, pt.name
+                  FROM mrp_production mp
+                  JOIN stock_move sm ON sm.production_id = mp.id
+                  JOIN stock_move smd ON smd.id = sm.move_dest_id
+                  JOIN stock_picking sp ON sp.id = smd.picking_id
+                  JOIN sale_order so ON so.id = mp.sale_id
+                  JOIN sale_order_type sot ON sot.id = so.type_id
+                  JOIN res_partner rp ON rp.id = mp.partner_id
+                  JOIN product_product pp ON pp.id = mp.product_id
+                  JOIN product_template pt ON pt.id = pp.product_tmpl_id
+                  WHERE sm.production_id = %s;
+            """, (mrp_id,))
+        dat = self.env.cr.fetchone()
+        data = {}
+        if dat:
+            data['date'] = dat[0]
+            data['type'] = dat[1]
+            data['partner_name'] = dat[2]
+            data['product_default'] = dat[3]
+            data['product_name'] = dat[4]
+        return data
+        #return ''
+
+    #def get_type(self, mrp_id):
+        #self.env.cr.execute("""SELECT sot.name
+                  #FROM mrp_production mp
+                  #JOIN sale_order so ON so.id = mp.sale_id
+                  #JOIN sale_order_type sot ON sot.id = so.type_id
+                  #WHERE mp.id = %s;
+            #""", (mrp_id,))
+        #types = self.env.cr.fetchone()
+        #if types:
+            #return types[0]
+        #return ''
+
+    #def get_partner_name(self, mrp_id):
+        #self.env.cr.execute("""SELECT rp.name
+                  #FROM mrp_production mp
+                  #JOIN res_partner rp ON rp.id = mp.partner_id
+                  #WHERE mp.id = %s;
+            #""", (mrp_id,))
+        #partners = self.env.cr.fetchone()
+        #if partners:
+            #return partners[0]
+        #return ''
+
+    #def get_product_name(self, mrp_id):
+        #self.env.cr.execute("""SELECT pp.name_template
+                  #FROM mrp_production mp
+                  #JOIN product_product pp ON pp.id = mp.product_id
+                  #WHERE mp.id = %s;
+            #""", (mrp_id,))
+        #names = self.env.cr.fetchone()
+        #if names:
+            #return names[0]
+        #return ''
+
+    #def get_product_default(self, mrp_id):
+        #self.env.cr.execute("""SELECT pp.default_code
+                  #FROM mrp_production mp
+                  #JOIN product_product pp ON pp.id = mp.product_id
+                  #WHERE mp.id = %s;
+            #""", (mrp_id,))
+        #names = self.env.cr.fetchone()
+        #if names:
+            #return names[0]
+        #return ''
 
     @api.model
     def render_html(self, docids, data=None):
@@ -88,8 +167,13 @@ class PrintLabelProd2Mrp(models.AbstractModel):
             'print_label.print_label_product2_mrp')
         docs = MrpProduction.browse(docids)
         docargs = {
-            'get_date': self.get_date,
+            'get_data': self.get_data,
+            #'get_date': self.get_date,
             'decimal_format': self.decimal_format,
+            #'get_type': self.get_type,
+            #'get_partner_name': self.get_partner_name,
+            #'get_product_name': self.get_product_name,
+            #'get_product_default': self.get_product_default,
             'doc_ids': docids,
             'doc_model': report.model,
             'docs': docs,
